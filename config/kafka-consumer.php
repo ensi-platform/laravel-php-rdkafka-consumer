299169eb4c0dab14859f79cc01337f6daa9a12dc
Nekrasov Ilya <nekrasov@greensight.ru>
2021-12-01 16:59:39 +0300

    Update kafka-consumer.php

--- config/kafka-consumer.php
+++ config/kafka-consumer.php
@@ -3,48 +3,7 @@
 return [
    'global_middleware' => [],
 
-   'processors' => [
-      [
-         /*
-         | Optional, defaults to `null`.
-         | Here you may specify which topic should be handled by this processor.
-         | Processor handles all topics by default.
-         */
-         'topic' => 'stage.crm.fact.registrations.1',
-
-         /*
-         | Optional, defaults to `null`.
-         | Here you may specify which ensi/laravel-phprdkafka consumer should be handled by this processor.
-         | Processor handles all consumers by default.
-         */
-         'consumer' => 'default',
-
-         /*
-         | Optional, defaults to `action`.
-         | Here you may specify processor's type. Defaults to `action`
-         | Supported types:
-         |  - `action` - a simple class with execute method;
-         |  - `job` - Laravel Queue Job. It will be dispatched using `dispatch` or `dispatchSync` method;
-         */
-         'type' => 'action',
-
-         /*
-         | Required.
-         | Fully qualified class name of a processor class.
-         */
-         'class' => \App\Domain\Communication\Actions\SendConfirmationEmailAction::class,
-
-         /*
-         | Optional, defaults to `false`.
-         | Proxy messages to Laravel's queue.
-         | Supported values:
-         |  - `false` - do not stream message. Execute processor in syncronous mode;
-         |  - `true` - stream message to Laravel's default queue;
-         |  - `<your-favorite-queue-name-as-string>` - stream message to this queue;
-         */
-         'queue' => false,
-      ],
-   ],
+   'processors' => [],
    
    'consumer_options' => [
       /** options for consumer with name `default` */
